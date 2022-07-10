4f2c4d21468a16e7ed979473a0b7509132f2f6b0
xiexiaopeng2002 <2020130293@mail.buct.edu.cn>
2022-07-10 09:32:16 +0800

    重构文件目录

--- src/views/Manage/index.tsx
+++ src/views/Manage/index.tsx
@@ -10,7 +10,7 @@ function Manageing() {
         <div className="list-box col-xs-12 col-md-3">
         <div className="list-group">
         <a  className="list-group-item active">导航栏</a>
-        <a href="Item file.html" className="list-group-item sub-nav">物品档案</a>
+        <a href="/manage/itemFile" className="list-group-item sub-nav">物品档案</a>
         <a href="Borrowing and returning records.html" className="list-group-item sub-nav">借还记录</a>
         <a href="Buy updates.html" className="list-group-item sub-nav">购买更新</a>
         <a href="Personnel management.html" className="list-group-item sub-nav">人员管理</a>
